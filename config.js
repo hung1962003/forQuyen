8de26a9234c03f02e41398cd758efa99caf09d46
hung1962003 <106923246+hung1962003@users.noreply.github.com>
2025-06-18 18:30:20 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,37 @@
+// const CONFIG = {
+//     titleWeb: "thế giới của bé:>>",
+//     introTitle: 'Bae à ',
+//     introDesc: `Trái đất vốn lạ thường
+//     Mà sao chị cứ đi nhầm đường
+//     Lạc vào tim bé lẻ loi
+//     Đằng sau chữ yêu đây là thương`,
+//     btnIntro: '^^HiHi^^',
+//     title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 🥰',
+//     desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
+//     btnYes: 'Yêu lắm luôn ah <33',
+//     btnNo: 'Bé nằm mơ sao :3',
+//     question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu mình bé thế <3',
+//     btnReply: 'Gửi cho bé nè <3',
+//     reply: 'Yêu thì yêu mà không yêu thì vẫn yêu thôi <33333333',
+//     mess: 'Bé biết mà 🥰. Hổng có chi nha Hổng có chi nha 🥰',
+//     messDesc: '19 ngày nữa chị có muốn đi ...cùng bé ko??? 👉👈.',
+//     btnAccept: 'Okiiiii lun <3',
+//     messLink: 'https://www.facebook.com/messages/t/100028912640019' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+// }
+const CONFIG = {
+    titleWeb: "Gửi Công túa capy",
+    introTitle: 'Kính gửi công túa',
+    introDesc: `From the day we met, you have been my exception`,
+    btnIntro: 'Chỗ Bóc Thư nè',
+    title: 'Phải chăng mình đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Tối nay công chúa đi dạo với thần nhó ',
+    btnYes: 'Oke đi thoaiii',
+    btnNo: 'Không đồ đáng ghét',
+    question: 'Choa tui gặp công túa đii, nhớ quá nhớ quá',
+    btnReply: 'Gửi cho tui  nè ❤',
+    reply: 'Chạy qua lẹ đi còn hỏi nữaaaa 😒',
+    mess: 'Bé biết mà 🥰. Chạy qua liền chạy qua liền (7h30) nhó 🥰',
+    messDesc: 'Tui wa liền ó nhoo 👉👈.',
+    btnAccept: 'Okiiiii lun ❤',
+    messLink :'https://www.facebook.com/messages/e2ee/t/6896117487091001'
+}
